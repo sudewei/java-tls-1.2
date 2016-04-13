f7492d3bc3f8e15147cb7dd1e43cae659c521dd9
Jiayu Yi (jiayu) <yi-jiayu@users.noreply.github.com>
2016-04-13 22:35:10 +0800

    Preemptively resolve merge conflicts with client side source

--- test/jiayu/tls/ClientTest.java
+++ test/jiayu/tls/ClientTest.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 public class ClientTest {
     @Test
     public void connectSecured() throws Exception {
-        Client client = new Client(new InetSocketAddress("localhost", 443));
+        Client client = new Client(new InetSocketAddress("192.168.148.129", 443));
         client.setCACertificate(Paths.get("misc/certs/servercert.crt"));
         client.connectSecured();
     }
