08e6a4491ad5a25e7d0251050e5b2fb9302f0af7
Jiayu Yi (jiayu) <yi-jiayu@users.noreply.github.com>
2016-04-13 23:56:54 +0800

    Push latest dev source

--- src/jiayu/tls/protocol/handshake/Certificate.java
+++ src/jiayu/tls/protocol/handshake/Certificate.java
@@ -6,6 +6,8 @@ import jiayu.tls.protocol.ProtocolMessage;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+// FIXME: 13/04/2016 IMPLEMENT CERTIFICATE CHAIN LENGTH AND CERTIFICATE LENGTH
+
 public class Certificate extends Handshake {
     private final int length;
     private final byte[] header;
@@ -27,6 +29,7 @@ public class Certificate extends Handshake {
 
     private byte[] toBytes() {
         return ByteBuffer.allocate(HEADER_LENGTH + length)
+                .put(header)
                 .put(certificateList)
                 .array();
     }
