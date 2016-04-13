51c16a776b5b74a057a72313f78bf1458b0f77e3
Jiayu Yi (jiayu) <yi-jiayu@users.noreply.github.com>
2016-04-13 23:48:08 +0800

    Implemented up to ServerHelloDone on client and server side

--- src/jiayu/tls/protocol/handshake/ClientHello.java
+++ src/jiayu/tls/protocol/handshake/ClientHello.java
@@ -3,7 +3,6 @@ package jiayu.tls.protocol.handshake;
 import jiayu.tls.protocol.ContentType;
 import jiayu.tls.protocol.ProtocolMessage;
 
-import javax.xml.bind.DatatypeConverter;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
@@ -122,35 +121,27 @@ public class ClientHello extends Handshake {
         if (message.getContentType() != ContentType.HANDSHAKE)
             throw new UnexpectedMessageException();
 
-        System.out.println(DatatypeConverter.printHexBinary(message.getContent()));
+        ByteBuffer content = ByteBuffer.wrap(message.getContent());
 
-        ByteBuffer buf = ByteBuffer.wrap(message.getContent());
+        int length = interpretHeader(content, HandshakeType.CLIENT_HELLO);  // get 4 bytes
 
-        byte type = buf.get();                                           // get 1 byte
-        if (HandshakeType.valueOf(type) != HandshakeType.CLIENT_HELLO)
-            throw new UnexpectedMessageException();
-
-        byte[] lengthBytes = new byte[3];
-        buf.get(lengthBytes);                                            // get 3 bytes
-        int length = UIntVector.btoi(lengthBytes);
-
-        short clientVersion = buf.getShort();                            // get 2 bytes
-        Random random = Random.fromBytes(buf);                           // get 32 bytes
-        byte sessionIdLength = buf.get();                                // get 1 byte
+        short clientVersion = content.getShort();                           // get 2 bytes
+        Random random = Random.fromBytes(content);                          // get 32 bytes
+        byte sessionIdLength = content.get();                               // get 1 byte
         byte[] sessionIdBytes = new byte[sessionIdLength];
-        buf.get(sessionIdBytes);                                         // get sessionid.length bytes
+        content.get(sessionIdBytes);                                        // get sessionid.length bytes
         UIntVector sessionId = new UIntVector(sessionIdBytes);
 
-        short cipherSuitesLength = buf.getShort();                       // get 2 bytes
+        short cipherSuitesLength = content.getShort();                      // get 2 bytes
         CipherSuite[] cipherSuites =
                 new CipherSuite[cipherSuitesLength / Short.BYTES];
 
-        for (int i = 0; i < cipherSuitesLength / Short.BYTES; i++)       // get ciphersuitelength bytes
-            cipherSuites[i] = CipherSuite.fromValue(buf.getShort());
+        for (int i = 0; i < cipherSuitesLength / Short.BYTES; i++)          // get ciphersuitelength bytes
+            cipherSuites[i] = CipherSuite.fromValue(content.getShort());
 
-        byte compressionMethodsLength = buf.get();                       // get 1 byte
+        byte compressionMethodsLength = content.get();                      // get 1 byte
         byte[] compressionMethods = new byte[compressionMethodsLength];
-        buf.get(compressionMethods);                                     // get 1 byte
+        content.get(compressionMethods);                                    // get 1 byte
 
         return new ClientHello(clientVersion, random, sessionId, cipherSuites, compressionMethods);
     }
@@ -160,52 +151,13 @@ public class ClientHello extends Handshake {
         return toBytes();
     }
 
-//    /**
-//     * Attempts to read a ClientHello message from a ReadableByteChannel.
-//     * <p>
-//     * Expects to receive only a ClientHello message, otherwise will throw an UnexpectedMessageException.
-//     *
-//     * @param src The channel to read from
-//     * @return A new ClientHello object
-//     * @throws IOException                If an I/O error occurs
-//     * @throws UnexpectedMessageException If an unexpected message was received
-//     */
-//    public static ClientHello tryToReadFrom(ReadableByteChannel src) throws IOException {
-//        Handshake handshake = Handshake.tryToReadFrom(src);
-//
-//        if (handshake.valueOf() != HandshakeType.CLIENT_HELLO) {
-//            throw new UnexpectedMessageException();
-//        }
-//
-//        return ClientHello.createFrom(handshake);
-//    }
-//
-//    private static ClientHello createFrom(Handshake handshake) {
-//        ByteBuffer buf = ByteBuffer.wrap(handshake.getContent());
-//
-//        short clientVersion = buf.getShort();
-//        Random random = Random.fromBytes(buf);
-//        int sessionId = buf.getInt();
-//        int cipherSuitesLength = buf.getInt();
-//        short[] cipherSuitesValues = new short[cipherSuitesLength / 2];
-//        for (int i = 0; i < cipherSuitesLength / 2; i++) {
-//            cipherSuitesValues[i] = buf.getShort();
-//        }
-//        List<CipherSuite> cipherSuites = CipherSuite.fromValues(cipherSuitesValues);
-//
-//        return new ClientHello(clientVersion, random, sessionId, cipherSuitesLength, cipherSuites);
-//    }
-
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("client_version: %s", Integer.toHexString(clientVersion)))
-                .append(String.format("random: %s", Arrays.toString(random.toBytes())))
-                .append(String.format("session_id: %d", sessionId.getValue()))
-                .append(String.format("cipher_suites", Arrays.toString(cipherSuites)))
-                .append(String.format("compression_methods: %s", Arrays.toString(compressionMethods)));
 
-        return sb.toString();
+        return String.format("client_version: %s", Integer.toHexString(clientVersion)) +
+                String.format("random: %s", Arrays.toString(random.toBytes())) +
+                String.format("session_id: %d", sessionId.getValue()) +
+                String.format("cipher_suites", Arrays.toString(cipherSuites)) +
+                String.format("compression_methods: %s", Arrays.toString(compressionMethods));
     }
 }
